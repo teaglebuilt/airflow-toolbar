b1266bb87bdd603696b7affd3bb136f66995a131
dillan teagle <dillan.teagle.va@gmail.com>
2020-12-03 07:47:52 -0600

    Update test_dashboard.py

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,16 +1,17 @@
-from airflow import settings
+from pathlib import Path
+from unittest.mock import patch, MagicMock
 from airflow.www import app as application
-from airflow.settings import Session
 from flask import config
-import jinja2
 import pytest
 
 
-def test_dashboard_view(mock_env):
-    settings.configure_orm()
+def test_dashboard_view(mock_airflow_db):
+    TEST_DB = "test.db"
+    BASE_DIR = Path(__file__).resolve().parent.parent
     app = application.create_app(testing=True)
     app.config['WTF_CSRF_ENABLED'] = False
-    app.jinja_env.undefined = jinja2.StrictUndefined
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
     client = app.test_client()
     resp = client.get('http://localhost:8080/admin/dashboardview/')
     assert resp.status_code == 200
